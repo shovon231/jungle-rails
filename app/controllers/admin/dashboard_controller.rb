c01746dfad501ff1ca39d62a14fa2d9483a7644f
Mohammad Razaul Islam <shovon231@gmail.com>
2023-09-19 17:34:47 +0000

    adding DB query

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,7 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV["ADMIN_USERNAME"], password: ENV["ADMIN_PASSWORD"]
   def show
+    @product = Product.all.count
+    @category = Category.all.count
   end
 end
